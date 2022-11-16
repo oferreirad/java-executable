e2dfcf724e37cc30e372ed669d07d851854c0c33
Diego Ferreira <diego.ferreira@paddypowerbetfair.com>
2022-11-16 12:32:22 +0000

    :sparkles: first commit

---
+++ src/main/java/com/diegoferreira/br/executable/ExecutableApplication.java
@@ -0,0 +1,13 @@
+package com.diegoferreira.br.executable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExecutableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExecutableApplication.class, args);
+	}
+
+}
